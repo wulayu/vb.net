744c3c54268df213185ef2e56573452a00aaf608
wulayu <849519573@qq.com>
2022-08-18 15:06:41 +0800

    init

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Up = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Down = New System.Windows.Forms.Button()
+        Me.Left = New System.Windows.Forms.Button()
+        Me.Right = New System.Windows.Forms.Button()
+        Me.Reset = New System.Windows.Forms.Button()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ZoomIn = New System.Windows.Forms.Button()
+        Me.ZoomOut = New System.Windows.Forms.Button()
+        Me.Label_Longitude = New System.Windows.Forms.Label()
+        Me.Label_ContextLongitude = New System.Windows.Forms.Label()
+        Me.Label_Latitude = New System.Windows.Forms.Label()
+        Me.Label_ContextLatitude = New System.Windows.Forms.Label()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Up
+        '
+        Me.Up.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Up.Location = New System.Drawing.Point(950, 171)
+        Me.Up.Name = "Up"
+        Me.Up.Size = New System.Drawing.Size(80, 45)
+        Me.Up.TabIndex = 1
+        Me.Up.Text = "Up"
+        Me.Up.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(956, 130)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 28)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Move :"
+        '
+        'Down
+        '
+        Me.Down.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Down.Location = New System.Drawing.Point(1045, 171)
+        Me.Down.Name = "Down"
+        Me.Down.Size = New System.Drawing.Size(80, 45)
+        Me.Down.TabIndex = 3
+        Me.Down.Text = "Down"
+        Me.Down.UseVisualStyleBackColor = True
+        '
+        'Left
+        '
+        Me.Left.Location = New System.Drawing.Point(950, 225)
+        Me.Left.Name = "Left"
+        Me.Left.Size = New System.Drawing.Size(80, 45)
+        Me.Left.TabIndex = 4
+        Me.Left.Text = "Left"
+        Me.Left.UseVisualStyleBackColor = True
+        '
+        'Right
+        '
+        Me.Right.Location = New System.Drawing.Point(1045, 225)
+        Me.Right.Name = "Right"
+        Me.Right.Size = New System.Drawing.Size(80, 45)
+        Me.Right.TabIndex = 5
+        Me.Right.Text = "Right"
+        Me.Right.UseVisualStyleBackColor = True
+        '
+        'Reset
+        '
+        Me.Reset.Location = New System.Drawing.Point(950, 275)
+        Me.Reset.Name = "Reset"
+        Me.Reset.Size = New System.Drawing.Size(175, 42)
+        Me.Reset.TabIndex = 6
+        Me.Reset.Text = "Reset"
+        Me.Reset.UseVisualStyleBackColor = True
+        '
+        'PictureBox
+        '
+        Me.PictureBox.Location = New System.Drawing.Point(34, 67)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(901, 450)
+        Me.PictureBox.TabIndex = 7
+        Me.PictureBox.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.Label2.Location = New System.Drawing.Point(34, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(197, 31)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "My World Map"
+        '
+        'ZoomIn
+        '
+        Me.ZoomIn.Location = New System.Drawing.Point(950, 399)
+        Me.ZoomIn.Name = "ZoomIn"
+        Me.ZoomIn.Size = New System.Drawing.Size(175, 42)
+        Me.ZoomIn.TabIndex = 9
+        Me.ZoomIn.Text = "ZoomIn"
+        Me.ZoomIn.UseVisualStyleBackColor = True
+        '
+        'ZoomOut
+        '
+        Me.ZoomOut.Location = New System.Drawing.Point(950, 447)
+        Me.ZoomOut.Name = "ZoomOut"
+        Me.ZoomOut.Size = New System.Drawing.Size(175, 42)
+        Me.ZoomOut.TabIndex = 10
+        Me.ZoomOut.Text = "ZoomOut"
+        Me.ZoomOut.UseVisualStyleBackColor = True
+        '
+        'Label_Longitude
+        '
+        Me.Label_Longitude.AutoSize = True
+        Me.Label_Longitude.Location = New System.Drawing.Point(34, 570)
+        Me.Label_Longitude.Name = "Label_Longitude"
+        Me.Label_Longitude.Size = New System.Drawing.Size(107, 24)
+        Me.Label_Longitude.TabIndex = 12
+        Me.Label_Longitude.Text = "Longitude :"
+        '
+        'Label_ContextLongitude
+        '
+        Me.Label_ContextLongitude.AutoSize = True
+        Me.Label_ContextLongitude.Location = New System.Drawing.Point(147, 570)
+        Me.Label_ContextLongitude.Name = "Label_ContextLongitude"
+        Me.Label_ContextLongitude.Size = New System.Drawing.Size(298, 24)
+        Me.Label_ContextLongitude.TabIndex = 13
+        Me.Label_ContextLongitude.Text = "请将鼠标悬浮于显示框中以查看经度"
+        '
+        'Label_Latitude
+        '
+        Me.Label_Latitude.AutoSize = True
+        Me.Label_Latitude.Location = New System.Drawing.Point(492, 570)
+        Me.Label_Latitude.Name = "Label_Latitude"
+        Me.Label_Latitude.Size = New System.Drawing.Size(90, 24)
+        Me.Label_Latitude.TabIndex = 14
+        Me.Label_Latitude.Text = "Latitude :"
+        '
+        'Label_ContextLatitude
+        '
+        Me.Label_ContextLatitude.AutoSize = True
+        Me.Label_ContextLatitude.Location = New System.Drawing.Point(597, 572)
+        Me.Label_ContextLatitude.Name = "Label_ContextLatitude"
+        Me.Label_ContextLatitude.Size = New System.Drawing.Size(298, 24)
+        Me.Label_ContextLatitude.TabIndex = 15
+        Me.Label_ContextLatitude.Text = "请将鼠标悬浮于显示框中以查看纬度"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1178, 644)
+        Me.Controls.Add(Me.Label_ContextLatitude)
+        Me.Controls.Add(Me.Label_Latitude)
+        Me.Controls.Add(Me.Label_ContextLongitude)
+        Me.Controls.Add(Me.Label_Longitude)
+        Me.Controls.Add(Me.ZoomOut)
+        Me.Controls.Add(Me.ZoomIn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox)
+        Me.Controls.Add(Me.Reset)
+        Me.Controls.Add(Me.Right)
+        Me.Controls.Add(Me.Left)
+        Me.Controls.Add(Me.Down)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Up)
+        Me.Name = "Form1"
+        Me.Text = "Made By Chen fanyun"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Up As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Down As Button
+    Friend WithEvents Left As Button
+    Friend WithEvents Right As Button
+    Friend WithEvents Reset As Button
+    Friend WithEvents PictureBox As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ZoomIn As Button
+    Friend WithEvents ZoomOut As Button
+    Friend WithEvents Label_Longitude As Label
+    Friend WithEvents Label_ContextLongitude As Label
+    Friend WithEvents Label_Latitude As Label
+    Friend WithEvents Label_ContextLatitude As Label
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,252 @@
+﻿Public Class Form1
+    Public ContinentList As New List(Of PointF()) '用于记录所有的点
+    Dim NowScale As Double
+    Dim OldScale As Double
+    Dim OriginPoint, Position As PointF
+    Dim MousePointer As PointF
+    Dim MinScale As Double = 1
+    Dim ZeroMoveVector As PointF
+    Dim NowOP As PointF
+    Dim RealToPixel As Double
+    'Dim LLScale As Double
+    Dim ZoomOrNot As Boolean
+    Dim ZoomMultiple As Double
+    Dim DisKey, TimeKey As Boolean
+    Dim DStartPoint, DEndPoint As PointF
+    Dim TPoint As PointF
+    Dim PointNum As Integer
+    Private Sub Initial()
+        OriginPoint.X = 574 \ 2 '地图中心原点
+        OriginPoint.Y = 286 \ 2
+        RealToPixel = 1.0 * 574 / 360
+        ZoomMultiple = 1.2
+        DisKey = False
+        TimeKey = False
+        ZoomOrNot = False
+        NowScale = 1 '设定最初的比例
+        OldScale = 1 '这里是为了ChangeOP函数的操作
+        PointNum = 0
+        ZeroMoveVector.X = 0
+        ZeroMoveVector.Y = 0
+        'WidthScale = 1
+        'HeightScale = 1
+        'MousePointer = OriginPoint
+    End Sub
+    Private Sub AppLoad(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+        Initial()
+        MapLoad("coastline.dat") '处理地图文件
+    End Sub
+    Private Sub MapLoad(Map As String)
+        Dim MapSrc As New IO.StreamReader(Map)
+        Dim ThisContinent As New List(Of PointF) '记录单个大洲
+        Dim ThisPoint As New PointF '记录单个点
+        Dim OneLineInMapFile As String() '临时用于存放一个点
+        Do Until MapSrc.EndOfStream() '直到MapSrc文件指针读到文件末尾
+            Dim i = MapSrc.ReadLine() 'i是字符串，文件中的一行
+            If i = "# -b" Or i = "" Then '如果读到一洲的末尾
+                If Not ThisContinent.Count().Equals(0) Then '如果这个洲不是空的（这个洲存在）
+                    ContinentList.Add(ThisContinent.ToArray()) '把这个洲附在记录所有点的List后面
+                    ThisContinent = New List(Of PointF) '将ThisContinent置成一个新List
+                End If
+            Else '如果没读到一个洲的末尾
+                OneLineInMapFile = Split(i, vbTab) '用这个OneLineInMapFile记录一下这个点的横纵坐标
+                '比例系数将经纬度翻译成像素
+                ThisPoint.X = OneLineInMapFile(0) * RealToPixel
+                ThisPoint.Y = -OneLineInMapFile(1) * RealToPixel
+                ThisContinent.Add(ThisPoint)
+            End If
+        Loop
+        DrawMap(NowScale, OriginPoint, ZeroMoveVector) '根据NowScale以原点为中心画图，此时NowScale为初始值
+    End Sub
+    Private Sub DrawMap(scale As Double, ZoomVector As PointF, MoveVector As PointF)
+        Dim BMP As New Drawing.Bitmap(PictureBox.Width, PictureBox.Height)
+        Dim GFX As Graphics = Graphics.FromImage(BMP)
+        GFX.TranslateTransform(0, 0)
+        GFX.FillRectangle(Brushes.White, 0, 0, PictureBox.Width, PictureBox.Height) '背景颜色填充
+        GFX.TranslateTransform(OriginPoint.X, OriginPoint.Y) '将图坐标原点设置为图框中心
+        Dim ThePen As New Pen(Color.Black, 0.1) '设定画笔数据
+        ChangeOP(scale, ZoomVector, MoveVector) '映射函数
+        For Each TempPoint In ContinentList
+            GFX.DrawLines(ThePen, TempPoint)
+        Next
+        If DisKey = True Then
+            ThePen = New Pen(Color.Red, 3)
+            If PointNum = 1 Then
+                GFX.DrawEllipse(ThePen, DStartPoint.X, DStartPoint.Y, 1, 1)
+            ElseIf PointNum = 2 Then
+                GFX.DrawEllipse(ThePen, DEndPoint.X, DEndPoint.Y, 1, 1)
+                GFX.DrawLine(ThePen, DStartPoint, DEndPoint)
+            End If
+        End If
+        PictureBox.Image = BMP
+    End Sub
+    Private Sub ChangeOP(scale As Double, ZoomVector As PointF, MoveVector As PointF)
+        NowOP.X = （scale / OldScale) * （0 - (ZoomVector.X - OriginPoint.X)) + MoveVector.X + (ZoomVector.X - OriginPoint.X)
+        NowOP.Y = （scale / OldScale) * （0 - (ZoomVector.Y - OriginPoint.Y)) + MoveVector.Y + (ZoomVector.Y - OriginPoint.Y)
+        'NowOP指初始中心点目前所在的像素点位
+        For Each TempPoint In ContinentList
+            Dim i As Integer = 0
+            For Each temp In TempPoint
+                '以vector为中心的放缩函数
+                '之前的点都是以0 0经纬度点为中心的
+                '现在更改为以vector经逆映射确定的点为中心
+                TempPoint(i).X = （scale / OldScale) * （TempPoint(i).X - (ZoomVector.X - OriginPoint.X)) + MoveVector.X + (ZoomVector.X - OriginPoint.X)
+                TempPoint(i).Y = （scale / OldScale) * （TempPoint(i).Y - (ZoomVector.Y - OriginPoint.Y)) + MoveVector.Y + (ZoomVector.Y - OriginPoint.Y)
+                'TempPoint(i).X = （scale / OldScale) * TempPoint(i).X + (ZoomVector.X - OriginPoint.X) * (1.0 / (scale / OldScale) - 1) + MoveVector.X
+                'TempPoint(i).Y = （scale / OldScale) * TempPoint(i).Y + (ZoomVector.Y - OriginPoint.Y) * (1.0 / (scale / OldScale) - 1) + MoveVector.Y
+                i += 1
+            Next
+        Next
+        'NewOriginalPoint = MousePointer - OriginPoint
+        'LLScale = scale / OldScale
+        OldScale = scale
+    End Sub
+
+    Function Longitude(NowMousePoint As PointF) As Double
+        Return ((NowMousePoint.X - OriginPoint.X - NowOP.X) / NowScale) / RealToPixel
+    End Function
+
+    Function Latitude(NowMousePoint As PointF) As Double
+        Return -((NowMousePoint.Y - OriginPoint.Y - NowOP.Y) / RealToPixel) / NowScale
+    End Function
+
+    Private Sub Up_Click(sender As Object, e As EventArgs) Handles Up.Click
+        Dim shift As Point
+        shift.Y = +30
+        DrawMap(NowScale, OriginPoint, shift)
+    End Sub
+
+    Private Sub Down_Click(sender As Object, e As EventArgs) Handles Down.Click
+        Dim shift As Point
+        shift.Y = -30
+        DrawMap(NowScale, OriginPoint, shift)
+    End Sub
+
+    Private Sub Left_Click(sender As Object, e As EventArgs) Handles Left.Click
+        Dim shift As Point
+        shift.X = +30
+        DrawMap(NowScale, OriginPoint, shift)
+    End Sub
+
+    Private Sub Right_Click(sender As Object, e As EventArgs) Handles Right.Click
+        Dim shift As Point
+        shift.X = -30
+        DrawMap(NowScale, OriginPoint, shift)
+    End Sub
+
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles Reset.Click
+        ContinentList.Clear()
+        Label_ContextLongitude.Text = "请将鼠标悬浮于显示框中以查看经度"
+        Label_ContextLatitude.Text = "请将鼠标悬浮于显示框中以查看纬度"
+        AppLoad(sender, e)
+    End Sub
+
+    Private Sub ZoomIn_Click(sender As Object, e As EventArgs) Handles ZoomIn.Click
+        NowScale = NowScale * ZoomMultiple
+        DrawMap(NowScale, OriginPoint, ZeroMoveVector)
+    End Sub
+
+    Private Sub ZoomOut_Click(sender As Object, e As EventArgs) Handles ZoomOut.Click
+        NowScale /= ZoomMultiple
+        If NowScale <= MinScale Then
+            NowScale = MinScale
+            ZoomOrNot = False
+        End If
+        DrawMap(NowScale, OriginPoint, ZeroMoveVector)
+    End Sub
+    Private Sub PictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox.MouseMove
+        MousePointer.X = e.X
+        MousePointer.Y = e.Y
+        'NameLatitude.Enabled = True
+        If ZoomOrNot = False Then
+            If Longitude(MousePointer) > 0 Then
+                Label_ContextLongitude.Text = "东经" + FormatNumber(Longitude(MousePointer), 2) + "°"
+            ElseIf Longitude(MousePointer) < 0 Then
+                Label_ContextLongitude.Text = "西经" + FormatNumber(-Longitude(MousePointer), 2) + "°"
+            Else
+                Label_ContextLongitude.Text = FormatNumber(Longitude(MousePointer), 2) + "°"
+            End If
+            If Latitude(MousePointer) > 0 Then
+                Label_ContextLatitude.Text = "北纬" + FormatNumber(Latitude(MousePointer), 2) + "°"
+            ElseIf Latitude(MousePointer) < 0 Then
+                Label_ContextLatitude.Text = "南纬" + FormatNumber(-Latitude(MousePointer), 2) + "°"
+            Else
+                Label_ContextLatitude.Text = FormatNumber(Latitude(MousePointer), 2) + "°"
+            End If
+        End If
+    End Sub
+
+    Private Sub Scroll_map(sender As Object, e As MouseEventArgs) Handles PictureBox.MouseWheel
+        If e.Delta > 0 Then
+            NowScale /= ZoomMultiple
+            If NowScale <= MinScale Then
+                NowScale = MinScale
+                ZoomOrNot = False
+            End If
+            DrawMap(NowScale, MousePointer, ZeroMoveVector)
+        ElseIf e.Delta < 0 Then
+            NowScale *= ZoomMultiple
+            DrawMap(NowScale, MousePointer, ZeroMoveVector)
+        End If
+    End Sub
+    Private Sub Move_map_begin(sender As Object, e As MouseEventArgs) Handles PictureBox.MouseDown
+        MousePointer.X = e.X
+        MousePointer.Y = e.Y
+        Position = MousePointer
+    End Sub
+    Private Sub Move_map_end(sender As Object, e As MouseEventArgs) Handles PictureBox.MouseUp
+        Dim vector As PointF
+        MousePointer.X = e.X
+        MousePointer.Y = e.Y
+        vector.X = MousePointer.X - Position.X
+        vector.Y = MousePointer.Y - Position.Y
+        DrawMap(NowScale, OriginPoint, vector)
+    End Sub
+    Private Sub Form1_Up(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Dim vec As Point
+        If e.KeyData = (Keys.W) Then
+            vec.Y = +30
+            DrawMap(NowScale, OriginPoint, vec)
+        End If
+    End Sub
+    Private Sub Form1_Down(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Dim vec As Point
+        If e.KeyData = (Keys.S) Then
+            vec.Y = -30
+            DrawMap(NowScale, OriginPoint, vec)
+        End If
+    End Sub
+    Private Sub Form1_Left(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Dim vec As Point
+        If e.KeyData = (Keys.A) Then
+            vec.X = +30
+            DrawMap(NowScale, OriginPoint, vec)
+        End If
+    End Sub
+    Private Sub Form1_Right(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Dim vec As Point
+        If e.KeyData = (Keys.D) Then
+            vec.X = -30
+            DrawMap(NowScale, OriginPoint, vec)
+        End If
+    End Sub
+    Private Sub Form1_Zoom_In(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyData = (Keys.J) Then
+            NowScale = NowScale * ZoomMultiple
+            DrawMap(NowScale, OriginPoint, ZeroMoveVector)
+        End If
+    End Sub
+    Private Sub Form1_Zoom_Out(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyData = (Keys.K) Then
+            NowScale /= ZoomMultiple
+            If NowScale <= MinScale Then
+                NowScale = MinScale
+                ZoomOrNot = False
+            End If
+            DrawMap(NowScale, OriginPoint, ZeroMoveVector)
+        End If
+    End Sub
+End Class
+
+
